a55ffd2236b9f15d23db7040223912d1b5ffe62d
codebene <114975279+rowooncoding@users.noreply.github.com>
2023-05-14 12:55:34 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,5 +1,12 @@
-
 def solution(array):
-    newArray = sorted(array)
-    result = len(newArray) // 2
-    return newArray[result]
+    median = 0
+    idx = 0
+    newArr = sorted(array)
+
+    if (len(newArr) % 2 == 0):
+        idx = len(newArr) // 2
+        median = (newArr[idx - 1] + array[idx]) / 2
+    else:
+        idx = len(newArr) // 2
+        median = newArr[idx]
+    return median
