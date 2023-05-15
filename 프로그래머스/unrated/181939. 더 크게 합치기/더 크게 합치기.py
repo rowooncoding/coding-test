6ad7cc3480ca5b74be481eb86de8bf70a119c595
codebene <114975279+rowooncoding@users.noreply.github.com>
2023-05-15 13:16:23 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,10 +1,2 @@
 def solution(a, b):
-    str1 = str(a)
-    str2 = str(b)
-    strAdd1 = str1 + str2
-    strAdd2 = str2 + str1
-
-    if (strAdd1 > strAdd2):
-        return int(strAdd1)
-    else:
-        return int(strAdd2)
+    return int(max(f"{a}{b}", f"{b}{a}"))
