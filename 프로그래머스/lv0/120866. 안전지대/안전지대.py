7cdf53c3ca5c72a99fa27684ad6bbac5bc1c29cc
codebene <114975279+rowooncoding@users.noreply.github.com>
2023-05-29 18:39:26 +0900

    [level 0] Title: 안전지대, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -0,0 +1,23 @@
+def solution(board):
+    answer = 0
+    N = len(board)
+    dx = [-1,1,0,0,-1,-1,1,1]
+    dy = [0,0,-1,1,-1,1,-1,1]
+    
+    z = []
+    for x in range(N):
+        for y in range(N):
+            if board[x][y] == 1:
+                z.append((x, y))
+                
+    for x, y in z:
+        for i in range(8):
+            nx = x + dx[i] 
+            ny = y + dy[i]
+            if 0<=nx<N and 0<=ny<N:
+                board[nx][ny] = 1           
+                
+    for b in board:
+        answer += b.count(0)
+    
+    return answer
