b3e542c3a062309690d7d32f662f663fe0fee788
codebene <114975279+rowooncoding@users.noreply.github.com>
2024-03-05 18:26:55 +0900

    [Bronze I] Title: 단어 공부, Time: 464 ms, Memory: 28860 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().toUpperCase();
+
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'A']++;
+        }
+
+        int maxCount = 0;
+        char maxAlphabet = '?';
+        for (int i = 0; i < 26; i++) {
+            if (maxCount < count[i]) {
+                maxCount = count[i];
+                maxAlphabet = (char) ('A' + i);
+            } else if (maxCount == count[i]) {
+                maxAlphabet = '?';
+            }
+        }
+
+        System.out.println(maxAlphabet);
+    }
+}
