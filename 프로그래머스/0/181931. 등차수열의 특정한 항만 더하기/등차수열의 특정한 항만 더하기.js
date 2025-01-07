fbac2ae71744e388703d5e749a51a0af19ab9763
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-01-07 10:36:19 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,11 @@
+function solution(a, d, included) {
+    let answer = 0;
+    
+    for (let i = 0; i < included.length; i++) {
+        if (included[i] === true) {
+            answer += a + (i * d);
+        }
+    }
+    
+    return answer;
+}
