d7d085c8ca7b9c4b13a98ae688ee73d04534be67
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-02-11 11:01:52 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,16 +1,3 @@
 function solution(my_string, is_suffix) {
-    let answer = 0;
-    
-    for (let i = 0 ; i < my_string.length ; i++) {
-        const sliceStr = my_string.slice(i);
-        
-        if (sliceStr === is_suffix) {
-            answer = 1;
-            break;
-        } else {
-            answer = 0;
-        }
-    }
-    
-    return answer;
+    return my_string.endsWith(is_suffix) ? 1 : 0;
 }
