28ee734c9de080ccf57a3a4b18f01a7ec76a69d6
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-02-19 10:33:43 +0900

    [level 0] Title: qr code, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -0,0 +1,10 @@
+function solution(q, r, code) {
+    let answer = '';
+    for (let i = 0 ; i < code.length ; i++) {
+        if (i % q === r) {
+            answer += code[i];
+        }
+    }
+    
+    return answer;
+}
