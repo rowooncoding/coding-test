104d8d7d8d2a78c872c61b1aecd6fd382e173a58
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-03-12 14:10:38 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,11 +1,7 @@
 function solution(str_list) {
-    const indexL = str_list.indexOf('l');
-    const indexR = str_list.indexOf('r');
-    
-    if (indexL !== -1 && (indexR === -1 || indexL < indexR)) {
-        return str_list.slice(0, indexL);
-    } else if (indexR !== -1) {
-        return str_list.slice(indexR + 1);
+    for (let i = 0 ; i < str_list.length ; i++) {
+        if (str_list[i] === 'l') return str_list.slice(0, i);
+        if (str_list[i] === 'r') return str_list.slice(i + 1);
     }
     
     return [];
