0d44a8888df022b119b89ee079c0ec8122fa4c2b
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-03-12 13:26:47 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/README.md
+++ 프로그래머스/0/181887. 홀수 vs 짝수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 홀수 vs 짝수 - 181887 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181887?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181887) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.18 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 13:07:52
+2025년 03월 12일 13:26:43
 
 ### 문제 설명
 
--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -2,13 +2,9 @@ function solution(num_list) {
     let sumOfOddNumber = 0;
     let sumOfEvenNumber = 0;
     
-    for (let i = 1 ; i <= num_list.length ; i++) {
-        if (i % 2 === 0) {
-            sumOfEvenNumber += num_list[i - 1];
-        } else {
-            sumOfOddNumber += num_list[i - 1];
-        }
-    }
-    
+    num_list.map((num, index) => {
+        index % 2 === 0 ? sumOfEvenNumber += num : sumOfOddNumber += num;
+    })
+     
     return sumOfOddNumber > sumOfEvenNumber ? sumOfOddNumber : sumOfEvenNumber
 }
