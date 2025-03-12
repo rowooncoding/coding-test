506a398ea63fe87a2fdf6cf472d2e6c0254a1cfb
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-03-12 14:00:36 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,12 @@
+function solution(str_list) {
+    const indexL = str_list.indexOf('l');
+    const indexR = str_list.indexOf('r');
+    
+    if (indexL !== -1 && (indexR === -1 || indexL < indexR)) {
+        return str_list.slice(0, indexL);
+    } else if (indexR !== -1) {
+        return str_list.slice(indexR + 1);
+    }
+    
+    return [];
+}
