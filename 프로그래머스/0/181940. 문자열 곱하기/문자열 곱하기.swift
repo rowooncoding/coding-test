3a4939828e13d5784546604e12df2afd8b4b3470
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-04-07 10:47:07 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.swift
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ k:Int) -> String {
-    var answer: String = ""
-    
-    for _ in 1...k {
-        answer += my_string
-    }
-    
-    return answer
+    return String(repeating: my_string, count: k)
 }
