e556035f01401c02138c25cf1fb4ee6f0a5b8111
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-04-07 10:31:10 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.10 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ arr:[String]) -> String {
+    var answer: String = ""
+    
+    for char in arr {
+        answer.append(char)
+    }
+    
+    return answer
+}
