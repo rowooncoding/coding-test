64ef45b50f2e5014ea318aba6a764c316414988e
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-04-09 09:35:49 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.swift
+++ 프로그래머스/0/181936. 공배수/공배수.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ number:Int, _ n:Int, _ m:Int) -> Int {
-    if number % n == 0 && number % m == 0 {
-        return 1
-    } else {
-        return 0
-    }
+    return number % n == 0 && number % m == 0 ? 1 : 0
 }
