0c22136df3d71966edb0a3afcda29c55146cea8d
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-08-25 15:40:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.25 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,20 @@
+def solution(num):
+    if num == 1:
+        return 0
+    
+    count = 0
+    
+    while num != 1:
+        count += 1
+        
+        if count == 500:
+            return -1
+        
+        if num % 2 == 0:
+            num= num / 2
+        elif num % 2 != 0:
+            num = num * 3 + 1
+    
+    return count
+    
+    
