bb05251cc7af521475529eaf9a3e78a25e2c0d56
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-09-09 10:41:43 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,13 @@
+def solution(d, budget):
+    d.sort()
+    
+    count = 0
+    
+    for amount in d:
+        if budget >= amount:
+            budget -= amount
+            count += 1
+        else:
+            break
+            
+    return count
