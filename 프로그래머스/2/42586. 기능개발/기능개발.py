d97f4163f2a1191ec54e37932364c96cdb15f51d
codebene <114975279+rowooncoding@users.noreply.github.com>
2025-12-10 10:07:37 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,22 +2,19 @@ import math
 
 def solution(progresses, speeds):
     answer = []
-    
     days = [math.ceil((100 - p) / s) for p, s in zip(progresses, speeds)]
     
-    count = 1
     max_day = days[0]
+    count = 1
     
     for i in range(1, len(days)):
-        if max_day >= days[i]:
+        if days[i] <= max_day:
             count += 1
         else:
             answer.append(count)
-            count = 1
             max_day = days[i]
+            count = 1
     
     answer.append(count)
     
     return answer
-            
-        
